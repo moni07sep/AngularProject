c259f73f2bc1feb6bb00c6872e85fa934c65a4e4
moni07sep <moni07sep@gmail.com>
2020-01-12 23:11:08 +0530

    Asynchronous load changes in navbr

--- appdata/src/app/shared/model/register.services.ts
+++ appdata/src/app/shared/model/register.services.ts
@@ -1,18 +1,30 @@
+
+import { routes } from './../../app.routes';
 import { Iregister, Ilogin } from './reg.interface';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
+import {Observable, BehaviorSubject} from 'rxjs'
+import { Router} from "@angular/router";
 
 @Injectable({providedIn:"root"})
 
 export class Register{
     public headers;
+
+    private loggedIn :BehaviorSubject<any>;
+    private userData :Observable<any>
+
+
     private url="http://mobile.test.acorsociety.com/ideators/api/users/userregistrationasync";
     private urlLogin="http://mobile.test.acorsociety.com/ideators/api/users/userloginasync";
-    constructor(private http:HttpClient ){
+    constructor(private http:HttpClient , private ro:Router ){
         this.headers= new HttpHeaders({
             "Content-Type":"application/json"
         })
+
+        this.loggedIn=new BehaviorSubject(JSON.parse(localStorage.getItem("currentuser")));
+        this.userData = this.loggedIn.asObservable();
     }
     userRegister(data:Iregister){
         return this.http.post(this.url, JSON.stringify(data),{headers:this.headers});
@@ -22,7 +34,8 @@ export class Register{
         return this.http.post(this.urlLogin, JSON.stringify(data),{headers:this.headers})
             .pipe(map((item:any)=>{
                 if(item && item.UserLogin.JwtToken){
-                    localStorage.setItem("currentUser", JSON.stringify(item))
+                    localStorage.setItem("currentUser", JSON.stringify(item));
+                    this.loggedIn.next(item);
                 }else{
                     return item;
                 }
@@ -32,5 +45,7 @@ export class Register{
     }
     logout(){
         localStorage.removeItem("currentUser");
+        this.loggedIn.next(null);
+        this.ro.navigateByUrl("/Login")
     }
 }
